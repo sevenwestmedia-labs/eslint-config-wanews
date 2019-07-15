aa01983cfa272bdebad5f17c29586d4fd29900ec
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-07-15 12:45:02 -0500

    Extend base config

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 "use strict";
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react-hooks"],
+  plugins: ["react-hooks"],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
+    "wanews-base",
   ],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
     "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies,
     "react/prop-types": 0
